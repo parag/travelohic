1b96a8b5590d7b14af3bbe62164b73246c41d315
Parag Arora <parag.arora@gmail.com>
2010-04-24 00:25:06 +0530

    session with fb

--- fburlmanager.php
+++ fburlmanager.php
@@ -6,7 +6,7 @@ $facebook = new Facebook(array('appId'=>FBAPPID, 'secret'=>FBAPPSECRET, 'cookie'
 session_start();
 
 $session = $facebook->getSession();
-$fbOAuthUrl = "https://graph.facebook.com/oauth/authorize?client_id=".FBAPPID."&redirect_uri=".base_url()."/fburlmanager.php&scope=email,user_birthday,user_hometown";
+//$fbOAuthUrl = "https://graph.facebook.com/oauth/authorize?client_id=".FBAPPID."&redirect_uri=".base_url()."/fburlmanager.php&scope=email,user_birthday,user_hometown";
 
 $me = null;
 // Session based API call.
@@ -24,7 +24,7 @@ if ($session)
 }
 else
 {
-    redirect($fbOAuthUrl);
+    die("fatal error. How did you land here?");
 }
 
 // login or logout url will be needed depending on current user state.
