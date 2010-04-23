2cd33a594ac4f1888ef30264188968e07c4f083b
Parag Arora <parag.arora@gmail.com>
2010-04-24 00:42:13 +0530

    session with fb

--- fburlmanager.php
+++ fburlmanager.php
@@ -8,7 +8,35 @@ $facebook = new Facebook(array('appId'=>FBAPPID, 'secret'=>FBAPPSECRET, 'cookie'
 session_start();
 
 $session = $facebook->getSession();
-//$fbOAuthUrl = "https://graph.facebook.com/oauth/authorize?client_id=".FBAPPID."&redirect_uri=".base_url()."/fburlmanager.php&scope=email,user_birthday,user_hometown";
+$fbOAuthUrl = "https://graph.facebook.com/oauth/authorize?client_id=".FBAPPID."&redirect_uri=".base_url()."/fburlmanager.php&scope=email,user_birthday,user_hometown";
 
-print_r($_COOKIE);
+$me = null;
+// Session based API call.
+if ($session)
+{
+    try
+    {
+        $uid = $facebook->getUser();
+        $me = $facebook->api('/me');
+		print_r($me);
+    }
+    catch(FacebookApiException $e)
+    {
+        error_log($e);
+    }
+}
+else
+{
+    redirect($fbOAuthUrl);
+}
+
+// login or logout url will be needed depending on current user state.
+if ($me)
+{
+    $logoutUrl = $facebook->getLogoutUrl(); echo $logoutUrl;
+}
+else
+{
+    $loginUrl = $facebook->getLoginUrl(); echo $loginUrl
+}
 ?>
