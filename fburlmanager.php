a1bb04c4135501c4ac1eaaeb466d8468ab6bbd4b
Parag Arora <parag.arora@gmail.com>
2010-04-24 00:43:55 +0530

    session with fb

--- fburlmanager.php
+++ fburlmanager.php
@@ -8,7 +8,7 @@ $facebook = new Facebook(array('appId'=>FBAPPID, 'secret'=>FBAPPSECRET, 'cookie'
 session_start();
 
 $session = $facebook->getSession();
-$fbOAuthUrl = "https://graph.facebook.com/oauth/authorize?client_id=".FBAPPID."&redirect_uri=".base_url()."/fburlmanager.php&scope=email,user_birthday,user_hometown";
+$fbOAuthUrl = "https://graph.facebook.com/oauth/authorize?client_id=".FBAPPID."&redirect_uri=http://travelohic.com/fburlmanager.php&scope=email,user_birthday,user_hometown";
 
 $me = null;
 // Session based API call.
