790f504a725e0ab755c1d50602741ec7ea311a70
Parag Arora <parag.arora@gmail.com>
2010-04-24 00:39:51 +0530

    session with fb

--- fburlmanager.php
+++ fburlmanager.php
@@ -9,6 +9,6 @@ session_start();
 
 $session = $facebook->getSession();
 //$fbOAuthUrl = "https://graph.facebook.com/oauth/authorize?client_id=".FBAPPID."&redirect_uri=".base_url()."/fburlmanager.php&scope=email,user_birthday,user_hometown";
-$me = $facebook->api('/me');
-print_r($me);
+
+print_r($_COOKIE);
 ?>
