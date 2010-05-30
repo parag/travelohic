3ee86296704b9dca6bf96fddb3d9a59ee29168af
unknown <Parag@.(none)>
2010-05-30 17:40:33 +0530

    tabbiong wishlist page

--- system/application/views/account/wishlist.php
+++ system/application/views/account/wishlist.php
@@ -10,6 +10,19 @@
 		<link rel="icon" href="<?=base_url()?>images/favicon.ico"> 
         <link rel="stylesheet" type="text/css" href="<?=base_url()?>css/reset-fonts-grid.css">
         <link rel="stylesheet" type="text/css" href="<?=base_url()?>css/messagescreen.css">
+        <link rel="stylesheet" type="text/css" href="<?=base_url()?>css/dark-hive/jquery-ui-1.8.1.custom.css">
+        <style>
+        // overwrite
+        	.ui-tabs { position: absolute; padding: .2em; zoom: 1; height: 400px;} /* position: relative prevents IE scroll bug (element with position: relative inside container with overflow: auto appear as "fixed") */
+			.ui-tabs .ui-tabs-nav { margin: 0; padding: .2em .2em 0; }
+			.ui-tabs .ui-tabs-nav li { list-style: none; float: left; position: relative; top: 1px; margin: 0 .2em 1px 0; border-bottom: 0 !important; padding: 0; white-space: nowrap; }
+			.ui-tabs .ui-tabs-nav li a { float: left; padding: .5em 1em; text-decoration: none; }
+			.ui-tabs .ui-tabs-nav li.ui-tabs-selected { margin-bottom: 0; padding-bottom: 1px; }
+			.ui-tabs .ui-tabs-nav li.ui-tabs-selected a, .ui-tabs .ui-tabs-nav li.ui-state-disabled a, .ui-tabs .ui-tabs-nav li.ui-state-processing a { cursor: text; }
+			.ui-tabs .ui-tabs-nav li a, .ui-tabs.ui-tabs-collapsible .ui-tabs-nav li.ui-tabs-selected a { cursor: pointer; } /* first selector in group seems obsolete, but required to overcome bug in Opera applying cursor: text overall if defined elsewhere... */
+			.ui-tabs .ui-tabs-panel { display: block; border-width: 0; padding: 1em 1.4em; background: none; height: 400px;}
+			.ui-tabs .ui-tabs-hide { display: none !important; }
+        </style>
     </head>
     <body>
     	<div id="left">
@@ -44,34 +57,44 @@
             </div>
             <div>
             <br/><br/><br/><br/><br/>
-        <h1>WISHLIST</h1>
-		<ul id="grid">
-		<?
-		$w = new Wishe();
-		$w->where('user_id', $a->id)->get();
-		$count = 0;
-		$wishes_mark = "[";
-		foreach($w->all as $wish)
-		{
-			$c = new Campaign();
-			$c->where('id', $wish->campaign_id)->get();
-			$pic = base_url()."images/bgsmall/".$c->photo;
-			$url = site_url('destination/index/'.$c->nickname);
-			echo "<li><a href=".$url."><img src=\"".$pic."\"/></a></li>";
-			if($count)
-				$wishes_mark = $wishes_mark.",";
-			$wishes_mark = $wishes_mark."[";
-			$wishes_mark = $wishes_mark."'".$c->name."', '".$c->description."', '".$url."', '".$pic."',".$c->lat.",".$c->lng;
-			$wishes_mark = $wishes_mark."]";
-			$count++;
-		}
-		$wishes_mark = $wishes_mark."]";
-		if($count==0)
-			echo "No destination added in wishlist yet.";
-		else
-			echo "Click on destination to add to your wishlist.";
-		?>
-		</ul>
+        <div id="tabs"> 
+			<ul> 
+				<li><a href="#wishlist"><img src="http://labs.google.com/ridefinder/images/mm_20_orange.png">WISHLIST</a></li> 
+				<li><a href="#tabs-2"><img src="http://labs.google.com/ridefinder/images/mm_20_green.png">VISITED</a></li> 
+				<li><a href="#tabs-3"><img src="http://labs.google.com/ridefinder/images/mm_20_purple.png">WISH TO VISIT AGAIN</a></li> 
+			</ul> 
+			<div id="wishlist">
+				<ul id="grid">
+				<?
+				$w = new Wishe();
+				$w->where('user_id', $a->id)->get();
+				$count = 0;
+				$wishes_mark = "[";
+				foreach($w->all as $wish)
+				{
+					$c = new Campaign();
+					$c->where('id', $wish->campaign_id)->get();
+					$pic = base_url()."images/bgsmall/".$c->photo;
+					$url = site_url('destination/index/'.$c->nickname);
+					echo "<li><a href=".$url."><img src=\"".$pic."\" height=50px/></a></li>";
+					if($count)
+						$wishes_mark = $wishes_mark.",";
+					$wishes_mark = $wishes_mark."[";
+					$wishes_mark = $wishes_mark."'".$c->name."', '".$c->description."', '".$url."', '".$pic."',".$c->lat.",".$c->lng;
+					$wishes_mark = $wishes_mark."]";
+					$count++;
+				}
+				$wishes_mark = $wishes_mark."]";
+				if($count==0)
+					echo "No destination added in wishlist yet.";
+				else
+					echo "Click on destination to add to your wishlist.";
+				?>
+				</ul>
+			</div> 
+			<div id="tabs-2">Phasellus mattis tincidunt nibh. Cras orci urna, blandit id, pretium vel, aliquet ornare, felis. Maecenas scelerisque sem non nisl. Fusce sed lorem in enim dictum bibendum.</div> 
+			<div id="tabs-3">Nam dui erat, auctor a, dignissim quis, sollicitudin eu, felis. Pellentesque nisi urna, interdum eget, sagittis et, consequat vestibulum, lacus. Mauris porttitor ullamcorper augue.</div> 
+		</div>
     </div>
 			
 	</div>
@@ -88,7 +111,12 @@
     </body>
 </html>
 <script type="text/javascript" src="http://maps.google.com/maps/api/js?sensor=true"></script>
+<script src="<?=base_url()?>js/jquery.min.js"></script>
+<script src="<?=base_url()?>js/jquery-ui.min.js"></script>
 <script type="text/javascript">
+$(function() {
+	$("#tabs").tabs();
+});
 var markers = <?php echo $wishes_mark; ?>;
 initialize();
   function initialize() {
@@ -106,7 +134,7 @@ initialize();
 	  for (var i=0; i<markers.length; i++){
 		  var marker = markers[i];
 		  var tLatLng = new google.maps.LatLng(marker[4], marker[5]);
-		  var image = new google.maps.MarkerImage('http://labs.google.com/ridefinder/images/mm_20_green.png',
+		  var image = new google.maps.MarkerImage('http://labs.google.com/ridefinder/images/mm_20_orange.png',
 			      // This marker is 20 pixels wide by 32 pixels tall.
 			      new google.maps.Size(20, 20),
 			      // The origin for this image is 0,0.
